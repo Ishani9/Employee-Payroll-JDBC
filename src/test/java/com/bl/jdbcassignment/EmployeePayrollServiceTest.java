ba1a1758552894e205c41710c70b5f470937a6cb
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-11-04 13:24:51 +0530

    Added: Ability to find sum, average, min, max and count by gender

--- src/test/java/com/bl/jdbcassignment/EmployeePayrollServiceTest.java
+++ src/test/java/com/bl/jdbcassignment/EmployeePayrollServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 import com.bl.jdbcassignment.EmployeePayrollService.IOService;
 
@@ -36,6 +37,10 @@ public class EmployeePayrollServiceTest {
 		assertTrue(result);
 	}
 	
+	/**
+	 * UC 5
+	 * 
+	 */
 	@Test
 	public void givenDateRange_WhenRetrieved_ShouldMatchEmployeeCount() {
 		List<EmployeePayrollData> employeeByDateList = null;
@@ -43,6 +48,45 @@ public class EmployeePayrollServiceTest {
 		LocalDate end = LocalDate.of(2020, 6, 12);
 		employeeByDateList = employeePayrollService.getEmployeeByDate(start, end);
 		assertEquals(3, employeeByDateList.size());
+	}
+	
+	/**
+	 * UC 6
+	 * 
+	 */
+	@Test
+	public void givenEmployees_WhenRetrievedAverage_ShouldReturnComputedMap() {
+		Map<String, Double> genderComputedMap = employeePayrollService.getEmployeeAverageByGender();
+		assertTrue(genderComputedMap.get("M").equals(350000.0));
+		assertTrue(genderComputedMap.get("F").equals(350000.0));
+	}
+
+	@Test
+	public void givenEmployees_WhenRetrievedMaximumSalaryByGender_ShouldReturnComputedMap() {
+		Map<String, Double> genderComputedMap = employeePayrollService.getEmployeeMaximumSalaryByGender();
+		assertTrue(genderComputedMap.get("M").equals(500000.0));
+		assertTrue(genderComputedMap.get("F").equals(400000.0));
+	}
+
+	@Test
+	public void givenEmployees_WhenRetrievedMinimumSalaryByGender_ShouldReturnComputedMap() {
+		Map<String, Double> genderComputedMap = employeePayrollService.getEmployeeMinimumSalaryByGender();
+		assertTrue(genderComputedMap.get("M").equals(200000.0));
+		assertTrue(genderComputedMap.get("F").equals(300000.0));
+	}
+
+	@Test
+	public void givenEmployees_WhenRetrievedSumByGender_ShouldReturnComputedMap() {
+		Map<String, Double> genderComputedMap = employeePayrollService.getEmployeeSumByGender();
+		assertTrue(genderComputedMap.get("M").equals(700000.0));
+		assertTrue(genderComputedMap.get("F").equals(700000.0));
+	}
+
+	@Test
+	public void givenEmployees_WhenRetrievedCountByGender_ShouldReturnComputedMap() {
+		Map<String, Double> genderComputedMap = employeePayrollService.getEmployeeCountByGender();
+		assertTrue(genderComputedMap.get("M").equals(2.0));
+		assertTrue(genderComputedMap.get("F").equals(2.0));
 	}
 }
 
