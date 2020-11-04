61caa115555d7d6fa4a8e05d3795d3f726487d77
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-11-04 12:35:02 +0530

    Refactored: Ability to update the salary using Prepared Statement

--- src/main/java/com/bl/jdbcassignment/EmployeePayrollDBService.java
+++ src/main/java/com/bl/jdbcassignment/EmployeePayrollDBService.java
@@ -2,23 +2,28 @@ package com.bl.jdbcassignment;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 public class EmployeePayrollDBService {
-	
 	private static EmployeePayrollDBService employeePayrollDBService;
-	private PreparedStatement employeePayrollDataStatement;
+	private PreparedStatement employeePayrollDataPrepareStatement;
 
-	public EmployeePayrollDBService() {
+	private EmployeePayrollDBService() {
 
 	}
 
+	/**
+	 * initiates employeePayrollDBService only once and returns same
+	 * 
+	 * @return
+	 */
 	public static EmployeePayrollDBService getInstance() {
 		if (employeePayrollDBService == null) {
 			employeePayrollDBService = new EmployeePayrollDBService();
@@ -26,101 +31,141 @@ public class EmployeePayrollDBService {
 		return employeePayrollDBService;
 	}
 
-	private Connection getConnection() 
-	{
+	/**
+	 * returns established connection with database
+	 * 
+	 * @return
+	 * @throws payrollServiceDBException
+	 */
+	private Connection getConnection() throws PayrollServiceDBException {
 		String jdbcURL = "jdbc:mysql://localhost:3306/emp_payroll?useSSL=false";
 		String userName = "root";
 		String password = "root";
 		Connection connection = null;
-		
-		try 
-		{
+
+		try {
 			System.out.println("Connecting to database:" + jdbcURL);
 			connection = DriverManager.getConnection(jdbcURL, userName, password);
-			System.out.println("Connection is successful!" + connection);					
+			System.out.println("Connection is successful!" + connection);
+		} catch (Exception exception) {
+			throw new PayrollServiceDBException("Connection is not successful");
 		}
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-		}
-		
+
 		return connection;
 	}
-	
-	public List<EmployeePayrollData> readData() throws SQLException
-	{
-		String sql = "select *  from employee_payroll;";
+
+	/**
+	 * returns list of 
+	 * @param resultSet
+	 * @return
+	 * @throws SQLException 
+	 * @throws payrollServiceDBException
+	 */
+	private List<EmployeePayrollData> getEmployeePayrollData(ResultSet resultSet) throws SQLException {
 		List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
-		
-		Connection connection = this.getConnection();
-		Statement statement = (Statement)connection.createStatement();
-		ResultSet resultSet = statement.executeQuery(sql);	
-		while(resultSet.next())
-		{
+		while (resultSet.next()) {
 			int id = resultSet.getInt("id");
 			String name = resultSet.getString("name");
 			double salary = resultSet.getDouble("salary");
 			LocalDate startDate = resultSet.getDate("start").toLocalDate();
 			employeePayrollList.add(new EmployeePayrollData(id, name, salary, startDate));
-		}	
-		return employeePayrollList;
-	}
+		}
 	
-	public int updateEmployeeData(String name, double salary) {
-		String sql = String.format("update employee_payroll set salary = %.2f where name = '%s';", salary, name);
-		try (Connection connection = this.getConnection()) {
-			Statement statement = (Statement) connection.createStatement();
-			return statement.executeUpdate(sql);
-		} 
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return 0;
-	}
-
-	public List<EmployeePayrollData> getEmployeePayrollData(String name) {
-		List<EmployeePayrollData> employeePayrollList = null;
-		if (this.employeePayrollDataStatement == null) {
-			this.prepareStatementForEmployeeData();
-		}
-
-		try {
-			employeePayrollDataStatement.setString(1, name);
-			ResultSet resultSet = employeePayrollDataStatement.executeQuery();
-			employeePayrollList = this.getEmployeePayrollData(resultSet);
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
 		return employeePayrollList;
 	}
 
-	private List<EmployeePayrollData> getEmployeePayrollData(ResultSet resultSet) {
-		List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
-
-		try (Connection connection = this.getConnection()) {
-			while (resultSet.next()) {
-				int id = resultSet.getInt("id");
-				String name = resultSet.getString("name");
-				double salary = resultSet.getDouble("salary");
-				LocalDate startDate = resultSet.getDate("start").toLocalDate();
-				employeePayrollList.add(new EmployeePayrollData(id, name, salary, startDate));
-			}
-		} 
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return employeePayrollList;
-	}
-
-	private void prepareStatementForEmployeeData() {
+	private void prepareStatementForEmployeeData() throws PayrollServiceDBException {
 		try {
 			Connection connection = this.getConnection();
 			String sql = "Select * from employee_payroll where name = ?";
-			employeePayrollDataStatement = (PreparedStatement) connection.prepareStatement(sql);
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
+			employeePayrollDataPrepareStatement = (PreparedStatement) connection.prepareStatement(sql);
+		} catch (SQLException | PayrollServiceDBException exception) {
+			throw new PayrollServiceDBException(exception.getMessage());
 		}
 	}
+	/**
+	 * updates data using statement
+	 * @param name
+	 * @param salary
+	 * @return
+	 * @throws payrollServiceDBException
+	 */
+	private int updateEmployeeDataUsingStatement(String name, double salary) throws PayrollServiceDBException {
+		String sql = String.format("update employee_payroll set salary = %.2f where name = '%s';", salary, name);
+
+		try (Connection connection = this.getConnection()) {
+			Statement statement = (Statement) connection.createStatement();
+			return statement.executeUpdate(sql);
+		} catch (PayrollServiceDBException exception) {
+			throw new PayrollServiceDBException(exception.getMessage());
+		} catch (SQLException exception) {
+			throw new PayrollServiceDBException("Error while updating data");
+		}
+	}
+	
+	/**
+	 * updates data using prepared statement
+	 * @param name
+	 * @param salary
+	 * @return
+	 * @throws payrollServiceDBException
+	 */
+	private int updateEmployeeDataUsingPreparedStatement(String name, double salary) throws PayrollServiceDBException {
+		try (Connection connection = this.getConnection()) {
+			String sql = "Update employee_payroll set salary = ? where name = ? ; " ; 
+			PreparedStatement prepareStatement = (PreparedStatement) connection.prepareStatement(sql);
+			prepareStatement.setDouble(1, salary);
+			prepareStatement.setString(2, name);
+			return prepareStatement.executeUpdate();
+		} catch (SQLException | PayrollServiceDBException exception) {
+			throw new PayrollServiceDBException(exception.getMessage());
+		}
+	}
+
+	/**
+	 * reads data from database and returns it in list
+	 * 
+	 * @return
+	 * @throws payrollServiceDBException
+	 */
+	public List<EmployeePayrollData> readData() throws PayrollServiceDBException {
+		String sql = "select *  from employee_payroll;";
+		List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+		try (Connection connection = this.getConnection()) {
+			Statement statement = (Statement) connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(sql);
+			employeePayrollList = this.getEmployeePayrollData(resultSet);
+		} catch (SQLException | PayrollServiceDBException exception) {
+			throw new PayrollServiceDBException(exception.getMessage());
+		}
+
+		return employeePayrollList;
+	}
+
+	/**
+	 * updates data and returns number of records updated
+	 * @param name
+	 * @param salary
+	 * @return
+	 * @throws payrollServiceDBException
+	 */
+	public int updateEmployeeData(String name, double salary) throws PayrollServiceDBException {
+		return this.updateEmployeeDataUsingPreparedStatement(name, salary);
+	}
+
+	public List<EmployeePayrollData> getEmployeePayrollData(String name) throws PayrollServiceDBException {
+		List<EmployeePayrollData> employeePayrollList = null;
+		try {
+			if (this.employeePayrollDataPrepareStatement == null) {
+				this.prepareStatementForEmployeeData();
+			}
+			employeePayrollDataPrepareStatement.setString(1, name);
+			ResultSet resultSet = employeePayrollDataPrepareStatement.executeQuery();
+			employeePayrollList = this.getEmployeePayrollData(resultSet);
+		} catch (SQLException | PayrollServiceDBException exception) {
+			throw new PayrollServiceDBException(exception.getMessage());
+		}
+		return employeePayrollList;
+	}
+
 }
--- src/main/java/com/bl/jdbcassignment/EmployeePayrollService.java
+++ src/main/java/com/bl/jdbcassignment/EmployeePayrollService.java
@@ -1,144 +1,88 @@
 package com.bl.jdbcassignment;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class EmployeePayrollService {
+	
 	public enum IOService {
 		CONSOLE_IO, FILE_IO, DB_IO, REST_IO
 	};
 
 	public List<EmployeePayrollData> employeePayrollList;
-	public EmployeePayrollDBService employeePayrollDBService;
+	private EmployeePayrollDBService employeePayrollDBService;
 
-	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
-		this.employeePayrollList = employeePayrollList;
-	}
-
+	/**
+	 * Default Constructor for caching employeePayrollDBService object 
+	 */
 	public EmployeePayrollService() {
 		employeePayrollDBService = EmployeePayrollDBService.getInstance();
 	}
 
-	static Scanner scanner = new Scanner(System.in);
-
-	public static void main(String[] args) throws SQLException {
-		ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
-		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
-
-		String choice = "yes";
-		do {
-			if (choice.equalsIgnoreCase("yes")) {
-				employeePayrollService.readEmployeeData(IOService.CONSOLE_IO);
-				scanner.nextLine();
-				System.out.println("Want to enter new employee payroll data?");
-				choice = scanner.nextLine();
-			}
-		} 
-		while (choice.equalsIgnoreCase("yes"));
-		employeePayrollService.writeEmployeeData(IOService.FILE_IO);
-		employeePayrollService.readEmployeeData(IOService.FILE_IO);
+	/**
+	 * returns employeePayrollData object given name of employee
+	 * 
+	 * @param name
+	 * @return
+	 */
+	private EmployeePayrollData getEmployeePayrollData(String name) {
+		EmployeePayrollData employeePayrollData = this.employeePayrollList.stream()
+				.filter(employeePayrollDataItem -> employeePayrollDataItem.name.equals(name)).findFirst().orElse(null);
+		return employeePayrollData;
 	}
 
 	/**
-	 * Takes employee input from user through console
+	 * reads employee data from database and returns list of employee payroll data
 	 * 
-	 * @param scanner
-	 * @return 
-	 * @throws SQLException 
+	 * @param ioService
+	 * @return
 	 */
-	public List<EmployeePayrollData> readEmployeeData(IOService ioService) throws SQLException {
-		if (ioService.equals(IOService.CONSOLE_IO)) {
-			System.out.println("Enter Employee ID : ");
-			int id = scanner.nextInt();
-			scanner.nextLine();
-			System.out.println("Enter Employee Name : ");
-			String name = scanner.nextLine();
-			System.out.println("Enter Employee Salary : ");
-			double salary = scanner.nextDouble();
-			employeePayrollList.add(new EmployeePayrollData(id, name, salary));
-		} 
-		else if (ioService.equals(IOService.FILE_IO)) {
-			System.out.println("reading data from file");
-			new EmployeePayrollFile().printData();
+	public List<EmployeePayrollData> readEmployeeData(IOService ioService) {
+		try {
+			if (ioService.equals(IOService.DB_IO)) {
+				this.employeePayrollList = employeePayrollDBService.readData();
+			}
+		} catch (PayrollServiceDBException exception) {
+			System.out.println(exception.getMessage());
 		}
-		else if (ioService.equals(IOService.DB_IO)) {
-			this.employeePayrollList = new EmployeePayrollDBService().readData();
-		}
-
 		return this.employeePayrollList;
 	}
 
 	/**
-	 * Writes data to console or file
-	 * 
-	 * @param ioService
-	 * @throws SQLException
-	 */
-	public void writeEmployeeData(IOService ioService) {
-		if (ioService.equals(IOService.CONSOLE_IO))
-			System.out.println("\nWriting Employee Payroll Roaster to Console\n" + employeePayrollList);
-		else if (ioService.equals(IOService.FILE_IO)) {
-			new EmployeePayrollFile().writeData(employeePayrollList);
-		}
-	}
-
-	/**
-	 * UC 3
+	 * given name and updated salary of employee updates in the database
 	 * 
 	 * @param name
 	 * @param salary
 	 */
 	public void updateEmployeePayrollSalary(String name, double salary) {
-		int result = employeePayrollDBService.updateEmployeeData(name, salary);
-
+		int result = 0;
+		try {
+			result = employeePayrollDBService.updateEmployeeData(name, salary);
+		} catch (PayrollServiceDBException exception) {
+			System.out.println(exception.getMessage());
+		}
 		if (result == 0) {
 			return;
 		}
-
 		EmployeePayrollData employeePayrollData = this.getEmployeePayrollData(name);
-		
 		if (employeePayrollData != null) {
 			employeePayrollData.salary = salary;
 		}
 	}
 
 	/**
-	 * UC 3
+	 * checks if record matches with the updated record 
 	 * 
 	 * @param name
-	 * @param salary
+	 * @return
 	 */
 	public boolean checkEmployeePayrollInSyncWithDB(String name) {
-		List<EmployeePayrollData> employeePayrollDataList = employeePayrollDBService.getEmployeePayrollData(name);
+		List<EmployeePayrollData> employeePayrollDataList = null;
+		try {
+			employeePayrollDataList = employeePayrollDBService.getEmployeePayrollData(name);
+		} catch (PayrollServiceDBException exception) {
+			System.out.println(exception.getMessage());
+		}
 		return employeePayrollDataList.get(0).equals(getEmployeePayrollData(name));
 	}
 
-	private EmployeePayrollData getEmployeePayrollData(String name) {
-		EmployeePayrollData employeePayrollData = this.employeePayrollList.stream()
-				.filter(employeePayrollDataItem -> employeePayrollDataItem.name.equals(name))
-				.findFirst().orElse(null);
-		return employeePayrollData;
-	}
-
-	
-	/**
-	 * Prints data from file to console
-	 * 
-	 * @param ioService
-	 */
-	public void printData(IOService ioService) {
-		if (ioService.equals(IOService.FILE_IO)) {
-			new EmployeePayrollFile().printData();
-		}
-	}
-
-	public long countEntries(IOService fileIo) {
-		long entries = 0;
-		if (fileIo.equals(IOService.FILE_IO)) {
-			entries = new EmployeePayrollFile().countEntries();
-		}
-		return entries;
-	}
 }
