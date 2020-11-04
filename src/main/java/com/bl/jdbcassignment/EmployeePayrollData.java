ddaae7bc51994940d26f52abbbc38eb6af4d1d11
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-11-04 20:20:40 +0530

    Added: Ability to work with ER Diagram

--- src/main/java/com/bl/jdbcassignment/EmployeePayrollData.java
+++ src/main/java/com/bl/jdbcassignment/EmployeePayrollData.java
@@ -1,6 +1,8 @@
+
 package com.bl.jdbcassignment;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class EmployeePayrollData {
 	public int id;
@@ -8,6 +10,8 @@ public class EmployeePayrollData {
 	public String gender;
 	public double salary;
 	public LocalDate startDate;
+	private List<String> departments;
+	public PayrollDetails payrollDetails;
 
 	public EmployeePayrollData(int id, String name, double salary) {
 		this.id = id;
@@ -24,9 +28,22 @@ public class EmployeePayrollData {
 		this(id, name, salary, startDate);
 		this.gender = gender;
 	}
+	
+	public EmployeePayrollData(int id, String name, String gender, double salary, LocalDate startDate,
+			List<String> departments) {
+		this(id, name, gender, salary, startDate);
+		this.departments = departments;
+	}
+
+	public EmployeePayrollData(int id, String name, String gender, double salary, LocalDate startDate, PayrollDetails payrollDetails,
+			List<String> departments) {
+		this(id, name, gender, salary, startDate, departments);
+		this.payrollDetails = payrollDetails;
+	}
 
 	public String toString() {
-		return "id = " + id + ", name = " + name + ", Salary = " + salary + ", Start Date = " + startDate;
+		return "id = " + id + ", Departments : " + departments + ", name = " + name + ", Gender = " + gender + ", Salary = " + salary + ", Start Date = "
+				+ startDate + ", Payroll Details = " + payrollDetails;
 	}
 
 	@Override
@@ -47,4 +64,4 @@ public class EmployeePayrollData {
 						Double.compare(that.salary, salary) == 0 &&
 						name.equals(that.name);
 	}
-}
+  
